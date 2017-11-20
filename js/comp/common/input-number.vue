b3b4841e76a5be3e13fe9fedb9a89b4b484a4d19
guoye <guoye@nmcc.ncr>
2017-11-20 10:39:36 +0800

    g

---
+++ js/comp/common/input-number.vue
@@ -0,0 +1,60 @@
+<template>
+<input type="text" :value="value" @input="onInput" />
+</template>
+
+<script>
+let methods = {};
+const fix = (v) => {
+	let n = parseFloat(v);
+	if (isNaN(n)) {
+		return 0;
+	}
+	if (v.charAt(v.length - 1) === '.') {
+		return n + '.';
+	}
+	return n;
+};
+methods.onInput = function () {
+	let oValue = this.$el.value.trim();
+	let val = fix(oValue);
+
+	if (this.numType === 'integer') {
+		val = parseInt(val, 10);
+	}
+
+	if (this.min !== undefined) {
+		if (val < this.min) {
+			val = this.min;
+		}
+	}
+	this.$el.value = val;
+	this.$emit('input', val);
+};
+let computed = {};
+let watch = {};
+const created = function () {};
+const mounted = function () {};
+const beforeDestroy = function () {};
+const dataFunc = function () {
+	let o = {};
+	return o;
+};
+export default {
+	data: dataFunc,
+	created,
+	methods,
+	computed,
+	watch,
+	props: ['value', 'numType', 'min'],
+	mounted,
+	mixins: [],
+	beforeDestroy,
+	components: {}
+};
+</script>
+
+<style scoped lang="less">
+.vue-comp {
+	
+}
+</style>
